a7636e6c45df20e5908d993607eb0730908d2ba5
Gerardo Hernandez <edgewl2@gmail.com>
2025-04-27 11:33:50 -0600

    build(gradle): nuevas dependencias para generar API

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.12.0"
 }
 
 group = "dev.edgeahz.ec.sports"
@@ -26,25 +27,41 @@ repositories {
 extra["springCloudVersion"] = "2024.0.1"
 
 dependencies {
+
+    //Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.flywaydb:flyway-mysql")
+
+    //Spring Cloud
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // Database
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.30")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
+    // Develop
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("com.h2database:h2")
 }
 
 dependencyManagement {
@@ -55,4 +72,39 @@ dependencyManagement {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$projectDir/src/main/resources/openapi/api.yaml")
+    outputDir.set("${layout.buildDirectory.get()}/generated")
+    apiPackage.set("dev.edgeahz.ec.sports.authserver.infrastructure.rest.api")
+    modelPackage.set("dev.edgeahz.ec.sports.authserver.infrastructure.rest.model")
+
+    additionalProperties.set(mapOf(
+        "additionalModelTypeAnnotations" to "@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)"
+    ))
+
+    configOptions.set(mapOf(
+        "interfaceOnly" to "true",
+        "delegatePattern" to "true",
+        "useSpringBoot3" to "true",
+        "returnSuccessCode" to "true",
+        "useTags" to "true",
+        "useOptional" to "false",
+        "dateLibrary" to "java8",
+        "serializationLibrary" to "jackson"
+    ))
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("${layout.buildDirectory.get()}/generated/src/main/java")
+        }
+    }
+}
+
+tasks.compileJava {
+    dependsOn(tasks.openApiGenerate)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("org.openapi.generator") version "7.12.0"
+    }
+}
+
 rootProject.name = "auth-server"
